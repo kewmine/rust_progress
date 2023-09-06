456fdd03b51176273ef2ab72c7ef2efc283b45ba
jek1 <0xjek1@gmail.com>
2023-09-06 22:38:47 +0530

    initial commit

---
+++ rustlings/exercises/functions/functions1.rs
@@ -0,0 +1,14 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+
+fn call_me() {
+    println!("Hello guys!");
+}
+
+
+fn main() {
+    call_me();
+}
+
+
